6cff31aadd0d1a9ab9f9c27c9c2639932804391e
Yury Borodich <xkxixnxgx@yandex.ru>
2022-04-29 21:00:41 +0300

    Added tests for recieve function and event Paid. Optimisation tests.

--- test/acceptingDonations.ts
+++ test/acceptingDonations.ts
@@ -20,16 +20,46 @@ describe('AcceptingDonations', function () {
     donations = (await deployContract(owner, AcceptingDonationsArtifact)) as AcceptingDonations;
   });
 
+  async function sendCoins(sender: any, amount: number ) {
+    const tsData: Object = {
+      to: donations.address,
+      value: amount,
+    }
+    const ts = await sender.sendTransaction(tsData);
+    await ts.wait();
+    return [ts, amount]
+  }
+
+
   describe('============ Contract deploy Test ============', () => {
     it("Correct smart contract address.", async function() {
       
       expect(donations.address).to.be.properAddress
-    })
+    });
 
     it("The balance of the expanded contract is 0.", async function() {
       const balance = await donations.currentBalance();
 
       expect(balance).to.eq(0);
+    });
+  });
+
+  describe('============= Event Paid Test =============', () => {
+    it("Send a donation to the contract wuthout function.", async function() {
+      const [sendCoinsTs, amount] = await sendCoins(addr1, 100);
+      await expect(() => sendCoinsTs).to.changeEtherBalances([donations, addr1], [amount, -amount]);
+    });
+
+    it("Checking the occurrence of an event and logging correct data.", async function() {
+      const [sendCoinsTs, amount] = await sendCoins(addr1, 100);
+      await expect(sendCoinsTs).to.emit(donations, "Paid").withArgs(addr1.address, amount);
+    });
+  });
+
+  describe('============= Recieve Test =============', () => {
+    it("Send a donation to the contract wuthout function.", async function() {
+      const [sendCoinsTs, amount] = await sendCoins(addr1, 100);
+      await expect(() => sendCoinsTs).to.changeEtherBalances([donations, addr1], [amount, -amount]);
     });
   });
 
@@ -40,7 +70,7 @@ describe('AcceptingDonations', function () {
       await transaction.wait();
 
       await expect(() => transaction).to.changeEtherBalances([addr1, donations], [-sum, sum]);
-    })
+    });
 
     it("Sending a donation of 0 coins.", async function() {
       const sum = 0;
@@ -52,10 +82,9 @@ describe('AcceptingDonations', function () {
   });
 
   describe('============= Coin withdrawal Test =============', () => {
-    it("Withdraw an arbitrary amount to your account.", async function() {
+    it("Withdraw an arbitrary amount to your (owner) account.", async function() {
       const sumForWithdraw = 50;
-      const transactionToContract = await donations.connect(addr1).transfer({value: 100});
-      await transactionToContract.wait();
+      await sendCoins(addr1, 100);
       const transactionToAnyAddr = await donations.connect(owner).withdraw(addr2.address, sumForWithdraw);
       await transactionToAnyAddr.wait();
 
@@ -69,22 +98,15 @@ describe('AcceptingDonations', function () {
       expect(balance).to.eq(50);
     });
 
-    // it("Only the contract owner can withdraw the amount.", async function() {
-    //   const sumForWithdraw = 50;
-    //   const transactionToContract = await donations.connect(addr1).transfer({value: 100});
-    //   await transactionToContract.wait();
-    //   const transactionToAnyAddr = await donations.connect(addr2).withdraw(addr2.address, sumForWithdraw);
-    //   await transactionToAnyAddr.wait();
+    it("Only the contract owner can withdraw the amount.", async function() {
+      const sumForWithdraw = 50;
+      await sendCoins(addr1, 100);
+      const balance = await donations.currentBalance();
 
-    //   await expect(() => transactionToAnyAddr).to.changeEtherBalances(
-    //     [donations, addr2], 
-    //     [0, 0]
-    //   );
-
-    //   const balance = await donations.currentBalance();
-
-    //   expect(balance).to.eq(100);
-    // });
+      await expect(donations.connect(addr2).withdraw(addr2.address, sumForWithdraw))
+        .to.be.revertedWith("Your address in not owner.");
+      expect(balance).to.eq(100);
+    });
 
   });
 
@@ -96,10 +118,8 @@ describe('AcceptingDonations', function () {
     });
 
     it("Address list of donors.", async function() {
-      const transactionOne = await donations.connect(addr1).transfer({value: 100});
-      await transactionOne.wait();
-      const transactionTwo = await donations.connect(addr2).transfer({value: 100});
-      await transactionTwo.wait();
+      await sendCoins(addr1, 100);
+      await sendCoins(addr2, 100);
       const accountsOfDonors = [addr1.address, addr2.address];
       const addressList = await donations.connect(addr3).getAccountsOfDonors();
 
@@ -107,10 +127,8 @@ describe('AcceptingDonations', function () {
     });
 
     it("Unique list of accounts.", async function() {
-      const transactionOne = await donations.connect(addr1).transfer({value: 100});
-      await transactionOne.wait();
-      const transactionTwo = await donations.connect(addr1).transfer({value: 100});
-      await transactionTwo.wait();
+      await sendCoins(addr1, 100);
+      await sendCoins(addr1, 100);
       const addressList = await donations.connect(addr3).getAccountsOfDonors();
 
       expect(addressList).to.have.lengthOf(1);
@@ -126,11 +144,8 @@ describe('AcceptingDonations', function () {
     });
 
     it("The sum of several donations", async function() {
-      const sumToContract = 100;
-      const transactionOne = await donations.connect(addr1).transfer({value: sumToContract});
-      await transactionOne.wait();
-      const transactionTwo = await donations.connect(addr1).transfer({value: sumToContract});
-      await transactionTwo.wait();
+      await sendCoins(addr1, 100);
+      await sendCoins(addr1, 100);
       const amountDonations = await donations.connect(addr2).amountOfAccountDonations(addr1.address);
 
       expect(amountDonations).to.eq(200);
@@ -139,10 +154,8 @@ describe('AcceptingDonations', function () {
     it("The amounts of donations from different accounts are not confused.", async function() {
       const sumToContractOne = 100;
       const sumToContractTwo = 200;
-      const transactionOne = await donations.connect(addr1).transfer({value: sumToContractOne});
-      await transactionOne.wait();
-      const transactionTwo = await donations.connect(addr2).transfer({value: sumToContractTwo});
-      await transactionTwo.wait();
+      await sendCoins(addr1, sumToContractOne);
+      await sendCoins(addr2, sumToContractTwo);
       const amountDonationsOne = await donations.connect(addr3).amountOfAccountDonations(addr1.address);
       const amountDonationsTwo = await donations.connect(addr3).amountOfAccountDonations(addr2.address);
 
