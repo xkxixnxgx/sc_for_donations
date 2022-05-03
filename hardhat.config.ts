1f388e79fc7b4038fff95f4410a6cc3a5d79131b
Yury Borodich <xkxixnxgx@yandex.ru>
2022-05-03 19:25:35 +0300

    Added tasks to hardhat.config.ts. Added rinkeby network to scripts/deploy.ts.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+import { BigNumber } from "@ethersproject/contracts/node_modules/@ethersproject/bignumber";
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -20,6 +21,146 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The smart contract's account")
+  .setAction(async (taskArgs, hre) => {
+    const accountStr: string = taskArgs.account;
+    const balance = await hre.ethers.provider.getBalance(accountStr);
+    console.log("Account:", accountStr);
+    console.log("Balance:", hre.ethers.utils.formatEther(balance), "ETH");
+});
+
+task("receive", "Send a donation")
+  .addParam("contract", "The smart contract's account")
+  .addParam("account", "The account's address")
+  .addParam("eth", "The coins donation")
+  .setAction(async (taskArgs, hre) => {
+    const accountSmartContract: string = taskArgs.contract;
+    const accAddress1: string = taskArgs.account;
+    const acc1: any = await hre.ethers.getSigner(accAddress1)
+    const valueTo: BigNumber = hre.ethers.utils.parseEther(taskArgs.eth);
+
+    const ts = {
+      to: accountSmartContract,
+      value: valueTo
+    }
+
+    const tsSend = await acc1.sendTransaction(ts);
+    await tsSend.wait()
+
+    const balance = await hre.ethers.provider.getBalance(accountSmartContract);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(balance), "ETH");
+  });
+
+task("transfer", "Send a donation")
+  .addParam("contract", "The smart contract's account")
+  .addParam("account", "The account's address")
+  .addParam("eth", "The coins donation")
+  .setAction(async (taskArgs, hre) => {
+    const accountSmartContract: string = taskArgs.contract;
+    const accAddress1: string = taskArgs.account;
+    const acc1: any = await hre.ethers.getSigner(accAddress1)
+    const valueTo: BigNumber = hre.ethers.utils.parseEther(taskArgs.eth);
+
+    const TransfersArtifact = require("./artifacts/contracts/AcceptingDonations.sol/AcceptingDonations.json")
+    
+    const transfersContract = new hre.ethers.Contract(
+      accountSmartContract,
+      TransfersArtifact.abi,
+      acc1
+    )
+
+    const ts = {
+      value: valueTo
+    }
+
+    await transfersContract.transfer(ts);
+
+    console.log(
+      "Transfer from ", accAddress1,
+      "to", accountSmartContract,
+      "coins in quantity", hre.ethers.utils.formatEther(valueTo).toString()
+      );
+    const balanceContract = await hre.ethers.provider.getBalance(accountSmartContract);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(balanceContract), "ETH");    
+  });
+
+task("withdraw", "Send a donation")
+  .addParam("contract", "The smart contract's account")
+  .addParam("owner", "The smart contract's owner")
+  .addParam("account", "The account's address")
+  .addParam("eth", "The coins donation")
+  .setAction(async (taskArgs, hre) => {
+    const accountSmartContract: string = taskArgs.contract;
+    const acc1Address: string = taskArgs.account;
+    const ownerAddress: string = taskArgs.owner;
+    const owner: any = await hre.ethers.getSigner(ownerAddress)
+    const acc1: any = await hre.ethers.getSigner(acc1Address)
+    const valueTo: BigNumber = hre.ethers.utils.parseEther(taskArgs.eth);
+    
+    const TransfersArtifact = require("./artifacts/contracts/AcceptingDonations.sol/AcceptingDonations.json");
+    const transfersContract = new hre.ethers.Contract(
+      accountSmartContract,
+      TransfersArtifact.abi,
+      owner
+    ) 
+
+    const result = await transfersContract.withdraw(acc1.address, valueTo);
+    console.log("Contract withdraw: ", result);
+});
+
+task("getAccountsOfDonors", "Send a donation")
+  .addParam("contract", "The smart contract's account")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const accountSmartContract: string = taskArgs.contract;
+    const accAddress1: string = taskArgs.account;
+    const acc1: any = await hre.ethers.getSigner(accAddress1)
+
+    const TransfersArtifact = require("./artifacts/contracts/AcceptingDonations.sol/AcceptingDonations.json");
+    const transfersContract = new hre.ethers.Contract(
+      accountSmartContract,
+      TransfersArtifact.abi,
+      acc1
+    ) 
+
+    const result = await transfersContract.getAccountsOfDonors();
+    if(result.lenght === 0) {
+      console.log("Not donation contracts.");
+    } else {
+      console.log("Donation contracts: ");
+      for(let i = 0; i < result.length; i += 1) {
+        console.log(result[i]);
+      }
+    }
+});
+
+task("amountOfAccountDonations", "Send a donation")
+  .addParam("contract", "The smart contract's account")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const accountSmartContract: string = taskArgs.contract;
+    const accAddress1: string = taskArgs.account;
+    const acc1: any = await hre.ethers.getSigner(accAddress1)
+
+    const TransfersArtifact = require("./artifacts/contracts/AcceptingDonations.sol/AcceptingDonations.json");
+    const transfersContract = new hre.ethers.Contract(
+      accountSmartContract,
+      TransfersArtifact.abi,
+      acc1
+    ) 
+
+    const result = await transfersContract.amountOfAccountDonations(accAddress1);
+    const resultStr: string = hre.ethers.utils.formatEther(result).toString();
+    if(resultStr === "0") {
+      console.log("Account", accAddress1, "doesn't donation to the smart contract.");
+      return
+    }
+    console.log("Donation contract: ", accAddress1);
+    console.log("ETH: ", resultStr);
+});
+
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,64 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+
+import { ethers, network, artifacts } from "hardhat";
+import * as fs from "fs";
+import * as path from "path";
+
+dotenv.config();
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const AcceptingDonations = await ethers.getContractFactory("AcceptingDonations");
+  if (network.name === "hardhat") {
+    console.warn("You are trying to deploy a contract to the Hardhat " + 
+    "gets automativally created and destroyed ever time. " + 
+    "Use the Hardhat option '--netwoork localhost'"
+    );
+  }
+
+  const [deployer] = await ethers.getSigners();
+
+  if (network.name === "rinkeby") {
+    const deployer = process.env.ACCOUNT_FOR_DEPLOY_TO_RINKEBY;
+  }
+
+  const AcceptingDonations = await ethers.getContractFactory("AcceptingDonations", deployer);
   const donations = await AcceptingDonations.deploy();
 
   await donations.deployed();
 
   console.log("AcceptingDonations deployed to:", donations.address);
+
+  saveFrontendFiles({
+    AcceptingDonations: donations
+  })
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+function saveFrontendFiles(contracts: any) {
+  const contractsDir: string = path.join(__dirname, '/..', 'front/contracts');
+
+  if(!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract]: [string, any] = contract_item;
+
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify({[name]: contract.address}, undefined, 2),
+      )
+    }
+
+    const ContractArtifact = artifacts.readArtifactSync(name)
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + '.json'),
+      JSON.stringify(ContractArtifact, null, 2),
+    )
+  })
+}
+
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
